1575ebc90b66887047805b0b7a5be75a4be3f98b
Matt Dear <mrmdear@gmail.com>
2019-11-20 15:17:21 +0000

    started writing tests for appointmentDao

--- myPracticeCourseWork/week6/jpaExample1/dao-jpa/src/test/java/org/solent/com504/jpaexample1/impl/dao/jpa/test/AppointmentDAOTest.java
+++ myPracticeCourseWork/week6/jpaExample1/dao-jpa/src/test/java/org/solent/com504/jpaexample1/impl/dao/jpa/test/AppointmentDAOTest.java
@@ -12,6 +12,9 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.solent.com504.jpaexample1.impl.dao.jpa.DAOFactoryJPAImpl;
 import org.solent.com504.jpaexample1.model.dao.AppointmentDAO;
+import org.solent.com504.jpaexample1.model.dto.Appointment;
+import org.solent.com504.jpaexample1.model.dto.Person;
+import org.solent.com504.jpaexample1.model.dto.Role;
 
 
 /**
@@ -30,6 +33,38 @@ public class AppointmentDAOTest {
     public void before() {
         appointmentDao = daoFactory.getAppointmentDAO();
         assertNotNull(appointmentDao);
+    }
+    
+    private void init() {
+    // delete all in database
+    personDao.deleteAll();
+    // add 5 patients
+    for (int i = 1; i < 6; i++) {
+        Person p = new Person();
+        p.setAddress("address_" + i);
+        p.setFirstName("firstName_" + i);
+        p.setSecondName("secondName_" + i);
+        p.setRole(Role.PATIENT);
+        personDao.save(p);
+        }
+    
+    person d = new Person();
+    p.setaddress();
+    
+    // delete all in database
+    appointmentDao.deleteAll();
+    // add 5 appointments
+    for (int i = 1; i < 6; i++) {
+        Appointment a = new Appointment();
+        a.setDescripton("Appointment_" + i);
+        a.setPersonA("personA_" + i);
+        a.setPersonB("personB_" + i);
+        a.setHr(i);
+        a.setMth(i);
+        a.setYr(i);
+        a.setDurationMinutes(i);      
+        appointmentDao.save(a);
+        }
     }
 
     @Test
